94cd3cbb2d131802cc16979c853be7ce45edf387
NAGARRO\yogesh05 <yogesh05@nagarro.com>
2024-03-08 15:46:09 +0530

    first commit

---
+++ src/test/java/com/work/Test1.java
@@ -0,0 +1,12 @@
+package com.work;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+   @Test
+    public void fun(){
+        System.out.println( "Hello World!!!!" );
+    }
+
+
+}
---
+++ src/test/java/com/work/Test2.java
@@ -0,0 +1,12 @@
+package com.work;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+   @Test
+    public void fun(){
+        System.out.println( "Hello World!!!!" );
+    }
+
+
+}
